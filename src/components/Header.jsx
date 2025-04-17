6140cc9e51b5ac36851b1e1d7c6cf5af3549cde7
Victor Santos <victor.csantos444@gmail.com>
2025-04-17 17:17:35 -0300

    Ajuste de icones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,12 @@ export default function Header() {
         <nav>
           <div className='header-elements'>
             <div className='header-elements-left'>
-              <a href="#light-toggle">modo escuro</a>
+              <input type="checkbox" id='checkboxInput' />
+                <label for="checkboxInput" class="toggleSwitch"></label>
             </div>
             <div className='header-elements-right'>
-              <a href="#ptbr">brazilzilzil</a>
-              <a href="#eng">engles</a>
+              <button href="#ptbr" className='portugues'><img src="/src/assets/brazil.png" alt="Português"/></button>
+              <button href="#eng" className='ingles'><img src="/src/assets/usa.png" alt="Inglês" /></button>
             </div>
           </div>
         </nav>
--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -25,10 +25,10 @@ export default function MainMenu() {
       <div className='intro-div'><p className="intro"><span className='oi'>Oi! </span><span className='victor'>Eu sou o Victor!!</span></p></div>
       <div className='subtext'><span>Dev Frontend | QA | Gostosa profissional</span></div>
       <div className="window-content">
-        <button onClick={() => openPopup('Sobre mim', <div>conteudo</div>)}> Sobre mim</button>
-        <button onClick={() => openPopup('Trabalho', <div>conteudo</div>)}>Trabalho</button>
-        <button onClick={() => openPopup('Contato', <div>conteudo</div>)}>Contato</button>
-        <button onClick={() => openPopup('Links', <div>conteudo</div>)}>Links</button>
+        <button className='btn-sobre' onClick={() => openPopup('Sobre mim', <div>conteudo</div>)}><img src="/src/assets/about.png" alt="Sobre"/><p>Sobre</p></button>
+        <button className='btn-trabalho' onClick={() => openPopup('Trabalho', <div>conteudo</div>)}><img src="/src/assets/project.png" alt="Trabalho"/><p>Trabalho</p></button>
+        <button className='btn-contato' onClick={() => openPopup('Contato', <div>conteudo</div>)}><img src="/src/assets/email.png" alt="Email"/><p>Contatos</p></button>
+        <button className='btn-links' onClick={() => openPopup('Links', <div>conteudo</div>)}><img src="/src/assets/link.png" alt="Links"/><p>Links</p></button>
       </div>
       {popups.map(popup => (
         <DraggablePopup 
