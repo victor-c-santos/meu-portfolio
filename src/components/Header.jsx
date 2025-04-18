9732f3f89369a54d17e85efe5f48978a7185175e
Victor Santos <victor.csantos444@gmail.com>
2025-04-17 22:57:18 -0300

    icons-hotfix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import '/src/styles/Header.css';
+import brazil from '/src/assets/brazil.png';
+import usa from '/src/assets/usa.png';
+
 // vai ser util no futuro, prometo
 export default function Header() {
     return (
@@ -11,8 +14,8 @@ export default function Header() {
                 <label for="checkboxInput" class="toggleSwitch"></label>
             </div>
             <div className='header-elements-right'>
-              <button href="#ptbr" className='portugues'><img src="./src/assets/brazil.png" alt="Português"/></button>
-              <button href="#eng" className='ingles'><img src="./src/assets/usa.png" alt="Inglês" /></button>
+              <button href="#ptbr" className='portugues'><img src={brazil} alt="Português"/></button>
+              <button href="#eng" className='ingles'><img src={usa} alt="Inglês" /></button>
             </div>
           </div>
         </nav>
