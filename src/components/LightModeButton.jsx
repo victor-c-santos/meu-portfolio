239862d220ea89caecb5c2a3228dec231f25d8a8
Victor Santos <victor.csantos444@gmail.com>
2025-04-23 18:27:49 -0300

    rework de colocação inicial dos popups

--- src/components/LightModeButton.jsx
+++ src/components/LightModeButton.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import '/src/styles/LightModeButton.css';
+import sun from '/src/assets/sun.png';
+import moon from '/src/assets/moon.png';
 
 export default function DarkmodeButton({ lightMode, setLightMode}) {
   return (
     <button onClick={() => setLightMode(!lightMode)} className="header-element">
-      {lightMode ? "🌙" : "☀️"}
+      {lightMode ? <img src={moon} alt="dark mode"/>: <img src={sun} alt="light mode" />}
     </button>
   );
 } 
