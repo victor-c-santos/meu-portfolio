836bb70f14db55cf7d130dd7519a54e8d5ca5b25
Victor Santos <victor.csantos444@gmail.com>
2025-04-24 16:48:01 -0300

    Rework do popup de projetos pronto

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -5,46 +5,48 @@ import imagemPortfolio from '/src/assets/portfolio.png';
 export default function Projetos() {
     return (
         <div className="main-container">
-            <div className="emprego-txt"><p>Estou atualmente aberto a ofertas de trabalhos/empregos, me contate pelo meu <a href="">email</a>.</p></div>
             <div className="ferramentas-desenvolvimento">
                 <div className="ferramentas">
                     <p className="txt-ferramentas txt-titulo">Ferramentas</p>
                     <div className="div-btn-ferramentas">
-                    <button className="btn-ferramentas">VSCode</button>
-                        <button className="btn-ferramentas">IntelliJ</button>
-                        <button className="btn-ferramentas">Github</button>
-                        <button className="btn-ferramentas">Jira</button>
-                        <button className="btn-ferramentas">Azure</button>
-                        <button className="btn-ferramentas">Postman</button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">VSCode</span></button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">IntelliJ</span></button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">Github</span></button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">Jira</span></button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">Azure</span></button>
+                        <button className="btn-ferramentas"><span className="btn-projetos-span">Postman</span></button>
                     </div>
                 </div>
                 <div className="desenvolvimento">
                     <p className="txt-desenvolvimento txt-titulo">Desenvolvimento</p>
                     <div className="div-btn-desenvolvimento">
-                        <button className="btn-desenvolvimento">Python</button>
-                        <button className="btn-desenvolvimento">Java</button>
-                        <button className="btn-desenvolvimento">JavaScript</button>
-                        <button className="btn-desenvolvimento">HTML/CSS</button>
-                        <button className="btn-desenvolvimento">React</button>
-                        <button className="btn-desenvolvimento">Cypress</button>
-                        <button className="btn-desenvolvimento">React</button>
-                        <button className="btn-desenvolvimento">Git</button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">Python</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">Java</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">JavaScript</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">HTML/CSS</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">React</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">Cypress</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">React</span></button>
+                        <button className="btn-desenvolvimento"><span className="btn-projetos-span">Git</span></button>
                     </div>
                 </div>
             </div>
             <div className="projetos">
-                    <p className="txt-projetos txt-titulo">Projetos</p>
+                    <p className="txt-projetos txt-titulo">Projetos públicos</p>
                     <div className="projeto projeto1">
                         <div className="projeto1-imagem">
                         <img src={imagemPortfolio} alt="" />
                         <div className="projeto1-texto">
                             <p className="projeto1-titulo">Portfolio</p>
                             <p className="projeto1-descricao">Página pessoal também usada como portfólio. Projeto feito com React, JavaScript e HTML/CSS.</p>
-                            <button className="btn-projeto1" onClick={() => window.open("https://github.com/victor-c-santos/meu-portfolio", "_blank")}>Github</button>
+                            <button className="btn-projeto1" onClick={() => window.open("https://github.com/victor-c-santos/meu-portfolio", "_blank")}><span className="btn-projetos-span">Github</span></button>
                         </div>
                         </div>
                     </div>
-                </div>
+                    <div className="projeto projeto2">
+                        <p className="projeto2-texto">Estou trabalhando em mais coisas mas por enquanto é isso..</p>
+                    </div>
+                </div>       
         </div>
     )
 }
