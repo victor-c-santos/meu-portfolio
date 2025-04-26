d7e5fe9cff06b09b493e6a9c5007fb2ea55d9c76
Victor Santos <victor.csantos444@gmail.com>
2025-04-26 14:29:39 -0300

    Rework dos ultimos 2 popups

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -6,10 +6,10 @@ import EmailCopiavel from '/src/components/EmailCopiavel.jsx'
 export default function Contato() {
     return (
         <div className="container-contato">
-            <p className="titulo">Entrar em contato comigo</p>
-            <p className="texto1">Geralmente eu não vejo mensagem pelo insta ou outras redes sociais, caso queira entrar em contato comigo, direcione suas mensagem ao meu email.</p>
+            <p className="titulo container-texto">Entrar em contato comigo</p>
+            <p className="texto1 container-texto">Geralmente eu não vejo mensagem pelo insta ou outras redes sociais, caso queira entrar em contato comigo, direcione suas mensagem ao meu email.</p>
             <img src={ astronauta } alt="" className="imagem-astronauta" />
-            <p className="texto2">Mande um email para <EmailCopiavel/><br />ou aperte o botão abaixo</p>
+            <p className="texto2 container-texto">Mande um email para <EmailCopiavel/><br />ou aperte o botão abaixo</p>
             <a href="mailto:victor.csantos444@gmail.com"><button className="btn-email">Mandar um email</button></a>
         </div>
     )
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import '/src/styles/Links.css';
-import github from '/src/assets/github.svg';
-import linkedin from '/src/assets/linkedin.svg';
-import instagram from '/src/assets/instagram.svg';
-import steam from '/src/assets/steam.svg';
-import backlog from '/src/assets/backlog.svg';
-import myanimelist from '/src/assets/myanimelist.svg';
+import github from '/src/assets/github.png';
+import linkedin from '/src/assets/linkedin.png';
+import instagram from '/src/assets/instagram.png';
+import steam from '/src/assets/steam.png';
 
 export default function Links() {
   return (
     <div className="botoes">
-        <button onClick={() => window.open("https://github.com/victor-c-santos", "_blank")}><img src={github}  alt="" className="icon-links"></img>Github</button>
-        <button onClick={() => window.open("https://www.linkedin.com/in/victor-hc-santos/", "_blank")}><img src={linkedin} alt="" className="icon-links" />LinkedIn</button>
-        <button onClick={() => window.open("https://www.instagram.com/ovictor.cs/#", "_blank")}><img src={instagram} alt="" className="icon-links" />Instagram</button>
-        <button onClick={() => window.open("https://steamcommunity.com/id/oVitrola/", "_blank")}><img src={steam} alt="" className="icon-links" />Steam</button>
-        <button onClick={() => window.open("https://docs.google.com/spreadsheets/d/1LBet-qxk0b4XnbJNl7WrSn9rKxX5Qv4C8bDYeEwoS2g/edit?gid=0#gid=0", "_blank")}><img src={backlog} alt="" className="icon-links" />Backlog</button>
-        <button onClick={() => window.open("https://myanimelist.net/profile/Laikea_H", "_blank")}><img src={myanimelist} alt="" className="icon-links" />MyAnimeList</button>
+        <button onClick={() => window.open("https://github.com/victor-c-santos", "_blank")}><img src={github}  alt="" className="icon-links-button icone-github"></img><p className="links-texto">Github</p></button>
+        <button onClick={() => window.open("https://www.linkedin.com/in/victor-hc-santos/", "_blank")}><img src={linkedin} alt="" className="icon-links-button icone-linkedin" /><p className="links-texto">LinkedIn</p></button>
+        <button onClick={() => window.open("https://www.instagram.com/ovictor.cs/#", "_blank")}><img src={instagram} alt="" className="icon-links-button icone-instagram" /><p className="links-texto">Instagram</p></button>
+        <button onClick={() => window.open("https://steamcommunity.com/id/oVitrola/", "_blank")}><img src={steam} alt="" className="icon-links-button icone-steam" /><p className="links-texto">Steam</p></button>
     </div>
   )
 }
